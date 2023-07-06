b3092a35ce5756910ae35238d4017ca02794bbd1
tbg8577 <41564253+tbg8577@users.noreply.github.com>
2023-07-06 17:57:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 import streamlit as st
+import pandas as pd
+import streamlit as st
+
+# view = [100, 150, 30]
+# view
+# st.bar_chart(view)
 
 public_gsheets_url = "https://docs.google.com/spreadsheets/d/1T50IexdJ9h4m7eetoo8JUfXfq1sb6mYBXFZiPUftAUQ/edit?usp=sharing"
+# public_gsheets_url
 
-view = [100, 150, 30]
-view
-st.bar_chart(view)
+# Read in data from the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
 
-public_gsheets_url
+df = load_data(st.secrets["public_gsheets_url"])
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
