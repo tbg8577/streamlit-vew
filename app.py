09a321ba1e49665c311848f747ff8dc9a91b1007
tbg8577 <41564253+tbg8577@users.noreply.github.com>
2023-07-06 18:24:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,15 @@ public_gsheets_url
 
 # Read in data from the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def load_data(sheets_url):
-    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url)
 
-df = load_data(st.secrets["public_gsheets_url"])
+# -------------------
+# @st.cache_data(ttl=600)
+# def load_data(sheets_url):
+#     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+#     return pd.read_csv(csv_url)
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+# df = load_data(st.secrets["public_gsheets_url"])
+
+# # Print results.
+# for row in df.itertuples():
+#     st.write(f"{row.name} has a :{row.pet}:")
