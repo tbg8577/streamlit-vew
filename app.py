bb579bc7b841558ccce75d70a2330cfc67ea8326
tbg8577 <41564253+tbg8577@users.noreply.github.com>
2023-07-06 18:26:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,13 @@ import streamlit as st
 # view
 # st.bar_chart(view)
 
-# public_gsheets_url = "https://docs.google.com/spreadsheets/d/1T50IexdJ9h4m7eetoo8JUfXfq1sb6mYBXFZiPUftAUQ/edit?usp=sharing"
-# public_gsheets_url
 st.secrets["public_gsheets_url"]
 
 # Read in data from the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 
 # -------------------
-# @st.cache_data(ttl=600)
+@st.cache_data(ttl=600)
 # def load_data(sheets_url):
 #     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
 #     return pd.read_csv(csv_url)
