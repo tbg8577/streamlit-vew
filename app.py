a98127bb30bd41b282907193d9b62b8587099102
tbg8577 <41564253+tbg8577@users.noreply.github.com>
2023-07-06 17:56:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-public_gsheets_url = https://docs.google.com/spreadsheets/d/1T50IexdJ9h4m7eetoo8JUfXfq1sb6mYBXFZiPUftAUQ/edit?usp=sharing
+public_gsheets_url = "https://docs.google.com/spreadsheets/d/1T50IexdJ9h4m7eetoo8JUfXfq1sb6mYBXFZiPUftAUQ/edit?usp=sharing"
 
 view = [100, 150, 30]
 view
