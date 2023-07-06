0608b7d4e1215e78131ddfce784147fe17cf83fd
tbg8577 <41564253+tbg8577@users.noreply.github.com>
2023-07-06 17:56:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+public_gsheets_url = https://docs.google.com/spreadsheets/d/1T50IexdJ9h4m7eetoo8JUfXfq1sb6mYBXFZiPUftAUQ/edit?usp=sharing
+
 view = [100, 150, 30]
 view
 st.bar_chart(view)
