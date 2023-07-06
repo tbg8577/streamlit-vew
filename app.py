c47a2a649c0b476dbf3e43009d9e681e66d95ae2
tbg8577 <41564253+tbg8577@users.noreply.github.com>
2023-07-06 18:26:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ st.secrets["public_gsheets_url"]
 
 # -------------------
 @st.cache_data(ttl=600)
-# def load_data(sheets_url):
-#     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-#     return pd.read_csv(csv_url)
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
 
 # df = load_data(st.secrets["public_gsheets_url"])
 
