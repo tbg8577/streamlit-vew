96ea79c91a3d10c81e00729f842b30dde1338977
tbg8577 <41564253+tbg8577@users.noreply.github.com>
2023-07-06 20:26:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import requests
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 
 # -------------------
-@st.cache_data(ttl=60)
+# @st.cache_data(ttl=60)
 # def load_data(sheets_url):
 #     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
 #     return pd.read_csv(csv_url)
